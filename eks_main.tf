1f4a60a552f36bb8bdf3bcd55ebb62c7dd206550
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2022-04-06 23:56:16 +0530

    Update eks_main.tf

--- eks_main.tf
+++ eks_main.tf
@@ -87,7 +87,6 @@ resource "aws_eks_node_group" "node" {
     desired_size = 1
     max_size     = 2
     min_size     = 1
-    key_name     = "admin"
   }
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
