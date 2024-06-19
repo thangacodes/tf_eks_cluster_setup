0a121e65db090426b5f3dc2b7021080fbbb7f24d
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-06-19 11:50:30 +0530

    Update eks_cluster.tf

--- kubernetes_setup/eks_cluster.tf
+++ kubernetes_setup/eks_cluster.tf
@@ -8,7 +8,7 @@ module "eks" {
   enable_irsa = true
 
   tags = {
-    cluster = "practice-demo-cluster"
+    cluster = "Thangadurai-eks-${random_string.suffix.result}"
   }
 
   vpc_id = module.vpc.vpc_id
