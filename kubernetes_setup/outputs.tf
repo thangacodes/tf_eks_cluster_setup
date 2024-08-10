feff78501ff0edd07e63d7e3d295b6a047dbb2d9
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-08-10 08:29:49 +0530

    updated

--- kubernetes_setup/outputs.tf
+++ kubernetes_setup/outputs.tf
@@ -1,28 +1,23 @@
-output "cluster_id" {
-  description = "EKS cluster ID."
-  value       = module.eks.cluster_id
+output "vpc_id" {
+  value = module.vpc.vpc_id
 }
 
-output "cluster_endpoint" {
-  description = "Endpoint for EKS control plane."
-  value       = module.eks.cluster_endpoint
+output "public_subnet_id" {
+  value = module.vpc.public_subnets
 }
 
-output "cluster_security_group_id" {
-  description = "Security group ids attached to the cluster control plane."
-  value       = module.eks.cluster_security_group_id
+output "private_subnet_id" {
+  value = module.vpc.private_subnets
 }
 
-output "region" {
-  description = "AWS region"
-  value       = var.aws_region
+output "vpc_cidr" {
+  value = module.vpc.vpc_cidr_block
 }
 
-output "oidc_provider_arn" {
-  value = module.eks.oidc_provider_arn
+output "endpoint" {
+  value = aws_eks_cluster.main.endpoint
 }
 
-#output "zz_update_kubeconfig_command" {
-  # value = "aws eks update-kubeconfig --name " + module.eks.cluster_id
-#  value = format("%s %s %s %s", "aws eks update-kubeconfig --name", module.eks.cluster_id, "--region", var.aws_region)
-#}
+output "kubeconfig-certificate-authority-data" {
+  value = aws_eks_cluster.main.certificate_authority[0].data
+}
