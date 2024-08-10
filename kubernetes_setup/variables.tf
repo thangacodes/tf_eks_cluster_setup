803f0cb4e8137250bcfaaf339b54ed3304e69f7d
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-08-10 08:30:52 +0530

    updated

--- kubernetes_setup/variables.tf
+++ kubernetes_setup/variables.tf
@@ -1,13 +1,3 @@
-variable "kubernetes_version" {
-  default     = 1.27
-  description = "kubernetes version"
-}
-
-variable "vpc_cidr" {
-  default     = "10.0.0.0/16"
-  description = "default CIDR range of the VPC"
-}
-variable "aws_region" {
-  default = "us-east-1"
-  description = "aws region"
-}
+variable "vpc_cidr" {}
+variable "public_subnet_cidr" {}
+variable "private_subnet_cidr" {}
