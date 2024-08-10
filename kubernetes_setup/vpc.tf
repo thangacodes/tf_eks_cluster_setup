6f68adbac0b8272450b6ec2218baf313dd74ef50
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-08-10 08:31:40 +0530

    updated

--- kubernetes_setup/vpc.tf
+++ kubernetes_setup/vpc.tf
@@ -1,44 +1,25 @@
-
-provider "aws" {
-  region = var.aws_region
+locals {
+  common_name = "eks-terraform"
 }
 
 data "aws_availability_zones" "available" {}
-
-locals {
-  cluster_name = "Thangadurai-eks-${random_string.suffix.result}"
-}
-
-resource "random_string" "suffix" {
-  length  = 5
-  special = false
+output "available_zones" {
+  value = data.aws_availability_zones.available.names
 }
 
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.7.0"
+  source = "terraform-aws-modules/vpc/aws"
 
-  name                 = "practice-eks-vpc"
-  cidr                 = var.vpc_cidr
-  azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24"]
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+  name = local.common_name
+  cidr = var.vpc_cidr
+
+  azs             = data.aws_availability_zones.available.names[*]
+  private_subnets = var.private_subnet_cidr
+  public_subnets  = var.public_subnet_cidr
+
+  enable_nat_gateway = true
 
   tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-  }
-
-  public_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                      = "1"
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"             = "1"
+    "kubernetes.io/cluster/${local.common_name}" = "shared"
   }
 }
